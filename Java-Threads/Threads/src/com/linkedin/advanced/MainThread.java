5d0c094b9881067d07c49be5a24a0cd5de20ef3d
audreypuckett <audreylynnpuckett@gmail.com>
2022-09-07 12:02:50 -0500

    add to MainThread

--- Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
@@ -3,7 +3,7 @@ package com.linkedin.advanced;
 public class MainThread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread().getName());
 
 	}
 
