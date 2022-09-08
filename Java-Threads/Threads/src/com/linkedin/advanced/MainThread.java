d7998f8e989f51beebc6962a9a1fdee97986eef3
audreypuckett <audreylynnpuckett@gmail.com>
2022-09-08 15:33:38 -0500

    join example

--- Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
@@ -10,7 +10,7 @@ public class MainThread {
 		// to illustrate how this is multi-threaded, we can place the creation and call to start into a for...loop  
 		
 		
-		// first example
+		// first example - creating threads
 //		for(int count = 1; count <= 5; count++) {
 //			FirstThread thread = new FirstThread(count);
 //			thread.start();
@@ -30,12 +30,31 @@ public class MainThread {
 //		}
 		
 		// Interrupting a thread
-		Thread taskThread = new Thread(new MyRunnable());
-		taskThread.start();
-		//Thread.sleep(3000);
-		taskThread.interrupt();
-		Thread.sleep(3000); // moving to give some time to the task thread before it is interrupted.
-		System.out.println(taskThread.isInterrupted());
+//		Thread taskThread = new Thread(new MyRunnable());
+//		taskThread.start();
+//		//Thread.sleep(3000);
+//		taskThread.interrupt();
+//		Thread.sleep(3000); // moving to give some time to the task thread before it is interrupted.
+//		System.out.println(taskThread.isInterrupted());
+		
+		
+		// Join method 
+		/*
+		 * If two threads are sharing resources, you can use the join method
+		 * to have the second thread wait while the first completes the initial use 
+		 * (e.g., changing a variable from null to containing a value)
+		 * When you do not specify when you want later threads to join, they will be ran in a random order and the 
+		 * output may not be what you intended.
+		 */
+		
+		//running two threads
+		
+		int[] marks = new int[10];
+		Thread marksThread = new Thread(new Marks(marks));
+		Thread averageThread = new Thread(new Average(marks));
+		marksThread.start();
+		marksThread.join();
+		averageThread.start();
 		
 	}
 
