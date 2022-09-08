5506d9f92bf8f8458809d69eda46e7b231fca12e
audreypuckett <audreylynnpuckett@gmail.com>
2022-09-07 20:30:10 -0500

    second chapter examples

--- Java-Threads/Threads/src/com/linkedin/advanced/FirstThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/FirstThread.java
@@ -1,5 +1,27 @@
 package com.linkedin.advanced;
 
 public class FirstThread extends Thread{
+	
+	private int number;
+	
+	public FirstThread(int number) {
+		this.number = number;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
 
+	@Override
+	public void run() {
+		for(int i = 1; i<=5; i++) {
+			System.out.println(i);
+			try {
+				sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
@@ -2,9 +2,22 @@ package com.linkedin.advanced;
 
 public class MainThread {
 
-	public static void main(String[] args) {
-		System.out.println(Thread.currentThread().getName());
+	public static void main(String[] args) throws InterruptedException {
+//		System.out.println(Thread.currentThread().getName());
+//		FirstThread thread1 = new FirstThread(); // Class extending Thread
+		// calling the run method directly would take this from being a thread to being a normal class
+//		thread1.start();
+		// to illustrate how this is multi-threaded, we can place the creation and call to start into a for...loop  
 		
+		for(int count = 1; count <= 5; count++) {
+			FirstThread thread = new FirstThread(count);
+			thread.start();
+			System.out.println("From thread " + thread.getNumber());
+		}
+//		
+//		MyRunnable runnable1 = new MyRunnable(); // Class implementing Runnable
+//		Thread thread = new Thread(runnable1);
+
 	}
 
 }
