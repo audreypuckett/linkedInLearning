e97e9f9dad63b935d5228c90bce769482a7a027f
audreypuckett <audreylynnpuckett@gmail.com>
2022-09-08 15:47:45 -0500

    other highuse methods for Threads

--- Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
@@ -5,9 +5,14 @@ public class MainThread {
 	public static void main(String[] args) throws InterruptedException {
 //		System.out.println(Thread.currentThread().getName());
 //		FirstThread thread1 = new FirstThread(); // Class extending Thread
-		// calling the run method directly would take this from being a thread to being a normal class
+		/*
+		 * calling the run method directly would take this from being a thread to being a normal class
+		 */
 //		thread1.start();
-		// to illustrate how this is multi-threaded, we can place the creation and call to start into a for...loop  
+		/*
+		 * to illustrate how this is multi-threaded, 
+		 * we can place the creation and call to start into a for...loop  
+		 */
 		
 		
 		// first example - creating threads
@@ -29,7 +34,7 @@ public class MainThread {
 //			}
 //		}
 		
-		// Interrupting a thread
+		// interrupt(), interrupted(), and isInterrupted()
 //		Thread taskThread = new Thread(new MyRunnable());
 //		taskThread.start();
 //		//Thread.sleep(3000);
@@ -38,7 +43,7 @@ public class MainThread {
 //		System.out.println(taskThread.isInterrupted());
 		
 		
-		// Join method 
+		// join()
 		/*
 		 * If two threads are sharing resources, you can use the join method
 		 * to have the second thread wait while the first completes the initial use 
@@ -47,15 +52,46 @@ public class MainThread {
 		 * output may not be what you intended.
 		 */
 		
-		//running two threads
+		//example of join
 		
-		int[] marks = new int[10];
-		Thread marksThread = new Thread(new Marks(marks));
-		Thread averageThread = new Thread(new Average(marks));
-		marksThread.start();
-		marksThread.join();
-		averageThread.start();
+//		int[] marks = new int[10];
+//		Thread marksThread = new Thread(new Marks(marks));
+//		Thread averageThread = new Thread(new Average(marks));
+//		marksThread.start();
+//		marksThread.join();
+//		averageThread.start();
 		
+		// getId()
+		
+		/*
+		 * Finds the unique identifier of a thread
+		 * public long getId()
+		 * Returns a positive number
+		 * ID is generated at the time of thread creation
+		 * ID can be used by another thread after the current thread using it is terminated.
+		 */
+		
+		// getName()
+		/*
+		 * Returns the name of the thread on with it's invoked
+		 */
+		
+		// getPriority()
+		
+		/*
+		 * Thread gets a priority at creation time
+		 * MIN_PRIORITY -1 and MAX_PRIORITY - 10
+		 * getPriority() gives the priority of a thread
+		 * Returns an int value between 1 and 10
+		 */
+		
+		// getState()
+		/*
+		 * returns the status
+		 *  -- New, Runnable, Blocked, Terminated, Timed_Waiting, Waiting
+		 *  Return value is an enum constant defined in the enum class Thread.State
+		 */
+	
 	}
 
 }
