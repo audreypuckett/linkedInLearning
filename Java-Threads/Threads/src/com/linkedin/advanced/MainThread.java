3544a4aecfcc63845d30c9a14a4f6e1d1a924df4
audreypuckett <audreylynnpuckett@gmail.com>
2022-10-03 10:34:36 -0500

    concurrent programming project created

--- Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
+++ Java-Threads/Threads/src/com/linkedin/advanced/MainThread.java
@@ -3,12 +3,12 @@ package com.linkedin.advanced;
 public class MainThread {
 
 	public static void main(String[] args) throws InterruptedException {
-//		System.out.println(Thread.currentThread().getName());
-//		FirstThread thread1 = new FirstThread(); // Class extending Thread
+		System.out.println(Thread.currentThread().getName());
+		FirstThread thread1 = new FirstThread(5); // Class extending Thread
 		/*
 		 * calling the run method directly would take this from being a thread to being a normal class
 		 */
-//		thread1.start();
+		thread1.start();
 		/*
 		 * to illustrate how this is multi-threaded, 
 		 * we can place the creation and call to start into a for...loop  
